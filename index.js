18279501d4c5cbae9530a73b0f10847b2e72876a
Jacob <jacobamelse@hotmail.com>
2022-07-19 04:17:11 -0500

    commit tree 498b77a10f9bb13eadbcb07363c37b3f9bbb341c

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(action){
+   if (action === undefined) {
+        return "This Saturday, I want to roller-skate!";
+    } else return `This Saturday, I want to ${action}!`
+}
+function mondayWork(action2 = "go to the office") {
+    return `This Monday, I will ${action2}.`
+}
+function wrapAdjective(flare = "*"){
+    return function(comp = "special"){
+        return `You are ${flare}${comp}${flare}!`
+    }
+}
